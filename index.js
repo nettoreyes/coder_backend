ecc517fe66bbffce9557933db3c1e934d436504b
nettoreyes <nettoreyes@gmail.com>
2021-12-15 22:35:27 -0300

    se agrega normalizr y desnormalizr

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express"); 
 const cors = require("cors");
 const faker = require('faker');
+const { normalize, denormalize, schema } = require('normalizr');
 faker.locale = "es_MX";
 
-const db_contenedor = require('./clases/db_contenedor');
-const db_contenedor_mensajes = require('./clases/db_contenedorMensajes');
+const db_contenedorArchivo = require('./clases/contenedor');
+const contenedorArchivo = new  db_contenedorArchivo("mensajes.txt");
+
 
 const { Server: IOServer, Socket } = require("socket.io");
 const { Server: HttpServer } = require('http');
@@ -36,28 +38,25 @@ app.use(express.static('./public'));
 
 io.on('connection',  async socket => {
     console.log("Conectados por socket");
-    try{
-        let productos_socket = await db_contenedor.listaProductos(); 
-        socket.emit("productos_server", productos_socket);
-    }catch(error){
-        //console.log("error ", error.code);
-        if(error.code === 'ER_NO_SUCH_TABLE')
-            await db_contenedor.creaTabla();
-        
-    }
+    
 
     try{
-        let listaMensajes = await db_contenedor_mensajes.listaMensajes(); 
+        let listaMensajes = await contenedorArchivo.getAll();  
         if(listaMensajes == false)
             listaMensajes = [];
-        io.sockets.emit("chat_server", listaMensajes); 
-    }catch(errorMensaje){        
-        if(errorMensaje.code === 'SQLITE_ERROR'){
-            await db_contenedor_mensajes.creaTabla();
 
-            listaMensajes = [];
-            io.sockets.emit("chat_server", listaMensajes); 
-        }
+        const mensajes = normalizaMensaje(listaMensajes)
+        //const respuesta = desNormalizaMensaje(mensajes);
+
+        io.sockets.emit("chat_server", mensajes); 
+    }catch(errorMensaje){       
+        console.log("error:", errorMensaje); 
+        // if(errorMensaje.code === 'SQLITE_ERROR'){
+        //     await db_contenedor_mensajes.creaTabla();
+
+        //     listaMensajes = [];
+        //     io.sockets.emit("chat_server", listaMensajes); 
+        // }
     }
     
     socket.on("notificacion", data => {
@@ -65,22 +64,55 @@ io.on('connection',  async socket => {
     })
 
     socket.on("input", async data => {
-        listaMensajes = await db_contenedor_mensajes.listaMensajes(); 
+        listaMensajes = await contenedorArchivo.getAll();  
         if(!listaMensajes)
             listaMensajes = [];
 
-        listaMensajes.push(data);
-        await db_contenedor_mensajes.guardaMensaje(listaMensajes);
-        io.sockets.emit("chat_server", listaMensajes); //notifica a todos 
-    })
 
-    socket.on("productos_cliente", async data => {        
-        productos_socket = await db_contenedor.listaProductos(); 
-        io.sockets.emit("productos_server", productos_socket); 
-    })
+        data.id = listaMensajes.length + 1;
+        listaMensajes.push(data);
+
+        await contenedorArchivo.save(listaMensajes);
+
+        const mensajes = normalizaMensaje(listaMensajes)
+
+        io.sockets.emit("chat_server", mensajes); //notifica a todos 
+    })  
 
     
 });
+
+const normalizaMensaje = (mensajes) => {
+    // defino el esquema de los autores
+    const authorSchema  = new schema.Entity("author", {}, { idAttribute:"email" });
+
+    const messageSchema = new schema.Entity('post', {author: authorSchema}, {idAttribute: 'id'});
+
+    const chatSchema = new schema.Entity('posts', { mensajes: [messageSchema] }, {idAttribute: 'id'});
+
+    const normalizedData = normalize({id: 'mismensajes', mensajes}, chatSchema);
+
+    // console.log(JSON.stringify(normalizedData, null, 3));
+
+    return normalizedData;
+}
+
+//prueba para desnormalizar
+const desNormalizaMensaje = (mensajes) => {
+    
+    console.log("********************DESNORMALIZA*********************")
+
+    const authorSchema  = new schema.Entity("author");
+    const messageSchema = new schema.Entity('post', {author: authorSchema});
+    const chatSchema = new schema.Entity('posts', { mensajes: [messageSchema] });   
+
+
+    const desnormalizedData = denormalize(mensajes.result, chatSchema ,mensajes.entities);
+
+    console.log(JSON.stringify(desnormalizedData, null, 3));
+
+    return desnormalizedData;
+}
 
 async function CargaProductos(){
     let productos_socket = await db_contenedor.listaProductos(); 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 const socket = io();
 const mensajes = document.querySelector('#misMensajes');
-const productosTabla = document.querySelector('#tablaProductos');
 
 
 //ingresar al chat
@@ -8,21 +7,36 @@ const ingresarChat = document.getElementById("btnIngresarChat");
 ingresarChat.addEventListener("click", () => { 
     var element = document.getElementById("zonaChat");
     element.classList.remove("d-none");
-    document.getElementById('email').readOnly = true;
+    document.getElementById('id').readOnly = true;
     ingresarChat.classList.add("d-none");
 });
 
 //enviar mensaje
 const input = document.getElementById("enviar_chat");
-input.addEventListener("click", () => { 
-    const usuario = document.getElementById("email").value;
-    const texto =  document.getElementById("mensaje").value;          
-    const fecha =  new Date().toLocaleString("es-CL");
+input.addEventListener("click", () => {   
+
+    const email = document.getElementById("id").value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const edad = document.getElementById("edad").value;
+    const alias = document.getElementById("alias").value;
+    const avatar = document.getElementById("avatar").value;
+    const text = document.getElementById("mensaje").value;
+    // const fecha =  new Date().toLocaleString("es-CL");
+
     const item = {
-        usuario,
-        texto,
-        fecha
-    }
+        id: 0,
+        author:{
+            email,
+            nombre,
+            apellido,
+            edad,
+            alias,
+            avatar
+        },
+        text
+    };
+
     socket.emit("input", item);  
     document.getElementById("mensaje").value = '';  
 });
@@ -30,67 +44,21 @@ input.addEventListener("click", () => {
 //capturo el contenido del chat
 socket.on("chat_server", data => {    
     mensajes.innerHTML = '';
-    if(data){
-        data.forEach(item => {
-            mensajes.innerHTML  += `<li><span class="text-primary"> ${ item.usuario } </span> <span class="text-muted fw-lighter">${ item.fecha }</span>  <span class="text-success fst-italic text-capitalize">${ item.texto }</span></li>`  
+    
+    const authorSchema  = new normalizr.schema.Entity("author");
+    const messageSchema = new normalizr.schema.Entity('post', {author: authorSchema});
+    const chatSchema = new normalizr.schema.Entity('posts', { mensajes: [messageSchema] }); 
+    const respuesta = normalizr.denormalize(data.result, chatSchema ,data.entities);
+
+    // console.log(JSON.stringify(respuesta.mensajes, null, 3));
+    const mensajeRespuesta = respuesta.mensajes;
+
+    if(mensajeRespuesta){
+        mensajeRespuesta.forEach(item => {
+            mensajes.innerHTML  += `<li><span class="text-primary"> ${ item.author.nombre } </span>   <span class="text-success fst-italic text-capitalize">${ item.text }</span></li>`  
         });   
     }
         
-});
-
-//guardo productos
-const creaProducto = document.getElementById("btnGuardaProducto");
-creaProducto.addEventListener("click", () => {
-
-    const title = document.querySelector('#title').value;
-    const price = document.querySelector('#price').value;
-    const thumbnail = document.querySelector('#thumbnail').value;
-
-    const nuevoProducto = {
-        title,
-        price,
-        thumbnail
-    };    
-
-    fetch('/',
-    {   
-        headers:{'content-type' : 'application/json'},
-        method: 'POST',
-        body : JSON.stringify(nuevoProducto)
-    }).then(resp => resp.json())
-    .then( data => {
-        console.log(data)
-        document.querySelector('#title').value = '';
-        document.querySelector('#price').value = '';
-        document.querySelector('#thumbnail').value = '';
-    }).catch( console.warn );
-
-});
-
-
-
-//cargo productos
-socket.on("productos_server", data => {    
-    productosTabla.innerHTML = ``;
-
-    if(data){
-        data.forEach(element => {
-            productosTabla.innerHTML += `<tr>
-                <th scope="row">${ element.id }</th>
-                <td>${ element.title }</td>
-                <td>${ element.price }</td>
-                <td><img src="${ element.thumbnail }" class="img-thumbnail" width="100" height="50"></td>
-            </tr>`
-        });
-    }
-    else{
-        productosTabla.innerHTML += `<tr>
-                <th class="text-center text-danger" colspan="4">No hay Productos!!!</th>                
-            </tr>`
-    }
-
-    
-    // console.table(data);
 });
 
 
