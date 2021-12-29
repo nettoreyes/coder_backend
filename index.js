d3bdde41d2b164748a4240ae7dcbd208040cd554
NettoReyes <nettoreyes@gmail.com>
2021-12-28 22:06:35 -0300

    inicio desafio InicioSesion

--- index.js
+++ index.js
@@ -45,13 +45,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(express.static('./public'));
 app.use(cookieParser());
-// app.use(session(
-//     {
-//         secret: 'secreto',
-//         resave: true,
-//         saveUninitialized: true
-//     }
-// ))
 
 app.use(session({
         store: MongoStore.create({            
